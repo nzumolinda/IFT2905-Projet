d3ce742d1cf2f96e96a4be6fc58c70fbd03ca51a
Rosalie C <roscla@hotmail.com>
2017-04-06 16:40:10 -0500

    Add Database package

---
+++ app/src/main/java/com/example/francoisluc/ift2905_projet/Database/Station.java
@@ -0,0 +1,18 @@
+package com.example.francoisluc.ift2905_projet.Database;
+
+/**
+ * Created by Rosalie on 2017-04-06.
+ */
+// Represent an item in table Stations.
+public class Station {
+
+    private int id;
+
+    public Station(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
--- app/src/main/java/com/example/francoisluc/ift2905_projet/FavoritesActivity.java
+++ app/src/main/java/com/example/francoisluc/ift2905_projet/FavoritesActivity.java
@@ -1,15 +1,23 @@
 package com.example.francoisluc.ift2905_projet;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
+
+import com.example.francoisluc.ift2905_projet.Database.Station;
+import com.example.francoisluc.ift2905_projet.Database.StationsDB;
 
 /**
  * Created by Rosalie on 2017-04-03.
  */
 public class FavoritesActivity extends AppCompatActivity {
+
+    private StationsDB db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +28,26 @@ public class FavoritesActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //
+        //This is a test
+        //
+        Station st  = new Station(2);
+        //Open database
+        db = new StationsDB(this);
+        db.open();
+        db.insertStation(st);
+
+        Cursor c = db.getStations();
+        if(c.getCount() != 0){
+           while(c.moveToNext()) {
+               int id = c.getInt(0);
+               Toast.makeText(this, "" + id, Toast.LENGTH_LONG).show();
+           }
+        }
+        //
+        // End of test
+        //
     }
 
     @Override
@@ -28,4 +56,5 @@ public class FavoritesActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.favorites_menu, menu);
         return true;
     }
+
 }
