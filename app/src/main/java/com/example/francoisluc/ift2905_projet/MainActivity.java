40792cfb260e2311d05f2933960356858714894b
Francois Luc Denhez-Teuton <flteuton@videotron.ca>
2017-04-26 19:30:43 -0400

    made the tabs work

--- app/src/main/java/com/example/francoisluc/ift2905_projet/MainActivity.java
+++ app/src/main/java/com/example/francoisluc/ift2905_projet/MainActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout mainhost;
+    ViewPager pager;
+    PagerAdapter pagerAdapter;
+    public final int NB_TABS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +31,45 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-            //public void onClick(View view) {
-              //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-            //}
-        //});
+        mainhost = (TabLayout) findViewById(R.id.tab_layout);
+        pager = (ViewPager)findViewById(R.id.pager);
+        pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+
+        mainhost.setupWithViewPager(pager);
+
+        mainhost.getTabAt(0).setIcon(R.drawable.ic_bicycle_pictogram);
+        mainhost.getTabAt(1).setIcon(R.drawable.ic_docks_30px);
+        mainhost.getTabAt(2).setText(R.string.itinerary_tab);
+
+
+
+    }
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        public PagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position){
+            SimpleFragment fragment = new SimpleFragment();
+            Bundle args =new Bundle();
+            args.putInt("id",position);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return NB_TABS;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return super.getPageTitle(position);
+        }
     }
 
     @Override
@@ -39,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.Favorite:
+                // Comportement du bouton "A Propos"
                 Intent intent = new Intent(getApplicationContext(), FavoritesActivity.class);
                 startActivity(intent);
                 return true;
